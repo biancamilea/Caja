e544d39e564d5b60df62e576031c3578ae240b1a
Bianca Milea <alumil2013@ieselcaminas.org>
2023-03-12 21:12:18 +0100

    Ioana Bianca Milea 8.12

--- src/Caja.java
+++ src/Caja.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Caja {
+public class Caja {
+        private final int ancho;
+        private final int alto;
+        private final int fondo;
+        private final Unidad unidad;
+        private String etiqueta;
+
+
+        public Caja(int ancho, int alto, int fondo, Unidad unidad) {
+            this.ancho = ancho;
+            this.alto = alto;
+            this.fondo = fondo;
+            this.unidad = unidad;
+        }
+
+        public double getVolumen() {
+            double volumen = ancho * alto * fondo;
+            if (unidad == Unidad.CM) {
+                volumen /= 1000000.0;
+            }
+            return volumen;
+        }
+
+        public void setEtiqueta(String etiqueta) {
+            this.etiqueta = etiqueta.substring(0, Math.min(etiqueta.length(), 30));
+        }
+
+        @Override
+        public String toString() {
+            return "Caja{" +
+                    "ancho=" + ancho +
+                    ", alto=" + alto +
+                    ", fondo=" + fondo +
+                    ", unidad=" + unidad +
+                    ", etiqueta='" + etiqueta + '\'' +
+                    '}';
+        }
 }
+
--- src/Unidad.java
+++ src/Unidad.java
@@ -1,2 +1,3 @@
-package PACKAGE_NAME;public enum Unidad {
+public enum Unidad {
+    CM, M
 }
